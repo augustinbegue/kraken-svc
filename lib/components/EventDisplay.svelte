952601e25698d5a3c6eb7cabec9631b65720504b
Augustin BÉGUÉ <augustin.begue@epita.fr>
2023-12-03 21:41:11 +0100

    feat(event): admin

---
+++ lib/components/EventDisplay.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { Event } from "@prisma/client";
+
+    export let event: Event;
+</script>
+
+<div class="card bg-base-100 shadow-xl">
+    <div class="card-body">
+        <h2 class="card-title">
+            {event.name}
+            <div class="badge badge-secondary">
+                {event.location}
+            </div>
+        </h2>
+        <span class="text-sm">
+            {event.startTime.toLocaleDateString()}
+            {event.startTime.toLocaleTimeString()} -{" "}
+            {event.endTime.toLocaleDateString()}
+            {event.endTime.toLocaleTimeString()}
+        </span>
+        <p>{@html event.description}</p>
+    </div>
+</div>
