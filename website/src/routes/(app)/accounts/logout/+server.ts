1a53e5745c51408bdd4ebb60f16d54bc7cb5bca2
Augustin BÉGUÉ <augustin.begue@epita.fr>
2023-12-20 10:24:40 +0100

    votez kraken

--- website/src/routes/(app)/accounts/logout/+server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-import type { RequestHandler } from "./$types";
-
-export const GET: RequestHandler = async ({ locals, url, cookies }) => {
-    cookies.delete("session", { path: "/" });
-    cookies.delete("state", { path: "/" });
-
-    throw redirect(302, "/");
-};
---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<div
+    class="mx-auto container p-4 h-screen flex flex-col justify-center items-center"
+>
+    <img src="/assets/kraken.png" class="max-w-sm" alt="" />
+    <a
+        href="https://moodle.epita.fr/course/view.php?id=2710"
+        class="text-center font-decorated text-8xl mt-4"
+    >
+        Votez Kraken
+    </a>
+    <a
+        href="https://moodle.epita.fr/course/view.php?id=2710"
+        class="btn btn-primary mt-4"
+    >
+        Moodle
+    </a>
+</div>
