a7eb019e7b39b597fc052b6a59f786b3ec1d01e2
Augustin Bégué <augustin.begue@epita.fr>
2024-10-27 20:15:13 +0100

    kraken: update env

--- lib/accounts/utils.ts
+++ lib/accounts/utils.ts
@@ -1,8 +1,7 @@
 import type { ClientSession } from ".";
-import { PUBLIC_API_URL } from '$env/static/public';
 
 export async function isLoggedIn(session: ClientSession): Promise<boolean> {
-    return session !== undefined && session.login !== undefined;
+    return !!session && !!session.login;
 }
 
 export function hasRole(profile: ClientSession, role: string): boolean {
