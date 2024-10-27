6994d57db806abcdc1152e915d9999ca04765304
Augustin Bégué <augustin.begue@epita.fr>
2024-10-27 21:06:17 +0100

    kraken: add reward

--- lib/server/accounts/utils.ts
+++ lib/server/accounts/utils.ts
@@ -7,6 +7,7 @@ export async function addReward(token: string): Promise<void> {
     }
 
     await fetch(new URL("/claim", process.env.API_URL), {
+        method: "POST",
         headers: {
             cookie: `krakookie=${token}`
         },
